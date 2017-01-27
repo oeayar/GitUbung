f538a626e503ca5864b735f8f20d46bd347b3c68
oeayar <oezgeayar@gmail.com>
2017-01-27 11:15:12 +0100

    hallo

--- app/src/main/java/com/example/oeayar/gitubung/MainActivity.java
+++ app/src/main/java/com/example/oeayar/gitubung/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.oeayar.gitubung;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnanezigen=(Button)findViewById(R.id.btnanzeigen);
+
+        btnanezigen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.daten);
+            }
+        });
+
 
     }
 }
