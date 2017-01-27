7f50f2fc5c9edb80d890dae9698ce15dcad7c9dd
oeayar <oezgeayar@gmail.com>
2017-01-27 10:40:17 +0100

    hallo

--- app/src/main/java/com/example/oeayar/gitubung/MainActivity.java
+++ app/src/main/java/com/example/oeayar/gitubung/MainActivity.java
@@ -9,9 +9,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String name;
+               String name;
         String hallo;
-
+String hrl;
         int zahl;
 
 
