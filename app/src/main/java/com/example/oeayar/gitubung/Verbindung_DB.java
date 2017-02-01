efe3487b01688b61d25445491510483874122d58
ozgeayar <oezgeayar@gmail.com>
2017-02-01 09:24:24 +0100

    Klasse Verbindung_DB

---
+++ app/src/main/java/com/example/oeayar/gitubung/Verbindung_DB.java
@@ -0,0 +1,20 @@
+package com.example.oeayar.gitubung;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by ozgeayar on 01.02.17.
+ */
+
+public class Verbindung_DB extends SQLiteOpenHelper {
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
